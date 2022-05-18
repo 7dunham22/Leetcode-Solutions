df28781c60a7cee88e80c42c72df778f1275e596
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-05-18 15:58:11 -0400

    Time: 102 ms (13.85%), Space: 41.9 MB (75.96%) - LeetHub

--- 796-rotate-string/796-rotate-string.js
+++ 796-rotate-string/796-rotate-string.js
@@ -7,14 +7,13 @@ var rotateString = function(s, goal) {
     if (s === goal) {
         return true;
     }
-    let i = 0;
-    const n = s.length;
-    while (i < n) {
-        s = s.slice(1) + s[0];
-        i++;
-        if (s === goal) {
+    let i = s.indexOf(goal[0], 0);
+    while (i !== -1) {
+        let s1 = s.slice(i) + s.slice(0,i);
+        if (s1 === goal) {
             return true;
         }
+        i = s.indexOf(goal[0], i+1);
     }
     return false;
 };
