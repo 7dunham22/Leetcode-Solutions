011393cd0f7afce55c71bcd6b959941d29107011
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-06-10 11:37:20 -0400

    Time: 7286 ms (5.15%), Space: 183 MB (5.00%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[][]} grid
+ * @param {number} k
+ * @return {number}
+ */
+var shortestPath = function(grid, k) {
+    const rEnd = grid.length-1;
+    const cEnd = grid[0].length-1;
+    const paths = [];
+    const queue = [[0,0,k,0, prev=new Set()]]; //[r,c,k,steps]
+    while (queue.length > 0) {
+        const curr = queue.shift();
+        const [r, c, x, steps, visited] = curr;
+        const key = r+','+c+','+x;
+        if (r === rEnd && c === cEnd) {
+            paths.push(steps);
+        } else if (!visited.has(key)){
+            visited.add(key);
+            const deltas = [[0,1], [1,0], [0,-1], [-1,0]];
+            for (let delta of deltas) {
+                const [dx, dy] = delta;
+                const rNext = r+dx;
+                const cNext = c+dy;
+                if (rNext >= 0 && rNext <= rEnd && cNext >= 0 && cNext <= cEnd) {
+                    if (grid[rNext][cNext] === 1 && x > 0) {
+                        queue.push([rNext, cNext, x-1, steps+1, visited]);
+                    } else if (grid[rNext][cNext] === 0){
+                        queue.push([rNext, cNext, x, steps+1, visited]);
+                    }
+                }
+            }   
+        }
+    }
+    if (paths.length === 0) return -1;
+    return Math.min(...paths);
+};
