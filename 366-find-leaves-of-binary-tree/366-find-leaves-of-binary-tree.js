e6adeb049678e0da7ab1669d08452ea3e5ce0988
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-06-10 16:47:52 -0400

    Time: 110 ms (9.12%), Space: 42 MB (84.87%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.js
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.js
@@ -11,49 +11,19 @@
  * @return {number[][]}
  */
 var findLeaves = function(root) {
-    const parents = getParents(root);
-    const res = [];
-    while (root) {
-        const leaves = getLeaves(root);
-        const level = [];
-        for (let leaf of leaves) {
-            const parent = parents.get(leaf);
-            if (parent && parent.left === leaf) {
-                parent.left = null;
-            } else if (parent) {
-                parent.right = null;
-            } else {
-                root = null;
-            }
-            level.push(leaf.val);
+    const res = {};
+    
+    const dfs = (node) => {
+        if (!node) return 0;
+        const depth = Math.max(dfs(node.left), dfs(node.right));
+        if (res[depth]) {
+            res[depth].push(node.val);
+        } else {
+            res[depth] = [node.val];
         }
-        res.push(level);
+        return depth + 1;
     }
-    return res; 
-};
-
-const getParents = (root) => {
-    const hash = new Map();
-    const queue = [root];
-    while (queue.length > 0) {
-        const curr = queue.shift();
-        if (curr.left) {
-            hash.set(curr.left, curr);
-            queue.push(curr.left);
-        }
-        if (curr.right) {
-            hash.set(curr.right, curr);
-            queue.push(curr.right);
-        }
-    }
-    return hash;
-}
-
-const getLeaves = (root) => {
-    if (!root) return [];
-    if (!root.left && !root.right) {
-        return [root];
-    }
-    const res = getLeaves(root.left).concat(getLeaves(root.right));
-    return res; 
+    
+    dfs(root);
+    return Object.values(res);
 }
