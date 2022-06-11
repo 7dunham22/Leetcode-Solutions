219799fd07274a2cce148bcd185500dbd9690ef6
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-06-11 16:02:32 -0400

    Time: 194 ms (74.47%), Space: 54.9 MB (32.77%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string[]} recipes
+ * @param {string[][]} ingredients
+ * @param {string[]} supplies
+ * @return {string[]}
+ */
+var findAllRecipes = function(recipes, ingredients, supplies) {
+    const res = [];
+	const available = new Set(supplies);
+	const ingredientToRecipes = {}; //Map of lacking ingredients:recipes.
+	const inDegree = {}; //Map of recipes : unavailable ingr count.
+	for (let i=0; i<recipes.length; i++) {
+		let unavailable = 0;
+        for (let ingr of ingredients[i]) {
+	        if (!available.has(ingr)) {
+		        if (!(ingr in ingredientToRecipes)) {
+	                ingredientToRecipes[ingr] = [recipes[i]];
+                } else {
+	                ingredientToRecipes[ingr].push(recipes[i]);
+                }
+                unavailable++;
+            }
+        }
+        if (unavailable === 0) {
+	        res.push(recipes[i]);
+        } else {
+	        inDegree[recipes[i]] = unavailable;
+        }	
+    }
+    for (let i=0; i<res.length; i++) {
+	    const recipe = res[i];
+	    if (recipe in ingredientToRecipes) {
+	        const lackingRecipes = ingredientToRecipes[recipe];
+	        for (let rec of lackingRecipes) {
+	            inDegree[rec]--;
+	            if (inDegree[rec] === 0) res.push(rec);
+            }
+        }
+    }
+    return res;
+
+};
