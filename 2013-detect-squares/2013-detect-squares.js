051efe99008ac22264aca872793ca4bee8c37d36
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-06-13 14:20:31 -0400

    Time: 290 ms (69.34%), Space: 51 MB (93.40%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.js
+++ 2013-detect-squares/2013-detect-squares.js
@@ -1,6 +1,6 @@
 
 var DetectSquares = function() {
-    this.points = {};
+    this.points = new Map();
     
 };
 
@@ -10,14 +10,14 @@ var DetectSquares = function() {
  */
 DetectSquares.prototype.add = function(point) {
     const [x, y] = point;
-    if (x in this.points) {
-        if (y in this.points[x]) {
-            this.points[x][y]++;
+    if (this.points.has(x)) {
+        if (this.points.get(x).has(y)) {
+            this.points.get(x).set(y, this.points.get(x).get(y)+1);
         } else {
-            this.points[x][y] = 1;
+            this.points.get(x).set(y, 1);
         }
     } else {
-        this.points[x] = {[y]: 1};
+        this.points.set(x, new Map().set(y, 1));
     }
 };
 
@@ -28,12 +28,12 @@ DetectSquares.prototype.add = function(point) {
 DetectSquares.prototype.count = function(point) {
     const [x1,y1] = point;
     let res = 0;
-    if (!(x1 in this.points)) return 0;
-    for (let x2 in this.points) {
-        for (let y2 in this.points[x2]) {
-            const first = this.points[x2][y2];
-            const second = this.points[x1][y2];
-            const third = this.points[x2][y1];
+    if (!(this.points.has(x1))) return 0;
+    for (let x2 of this.points.keys()) {
+        for (let y2 of this.points.get(x2).keys()) {
+            const first = this.points.get(x2).get(y2);
+            const second = this.points.get(x1).get(y2);
+            const third = this.points.get(x2).get(y1);
             if (Math.abs(x1-x2) === 0 || Math.abs(x1-x2) !== Math.abs(y1 - y2) || !second|| !third) continue;
             res += first * second * third;
         }
