907262e07a2172c62ba5dd650dec110914ea7561
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-06-13 14:02:07 -0400

    Time: 1176 ms (8.49%), Space: 89.1 MB (8.49%) - LeetHub

---
+++ 2013-detect-squares/2013-detect-squares.js
@@ -0,0 +1,47 @@
+
+var DetectSquares = function() {
+    this.points = {};
+    
+};
+
+/** 
+ * @param {number[]} point
+ * @return {void}
+ */
+DetectSquares.prototype.add = function(point) {
+    const [x, y] = point;
+    if (x in this.points) {
+        if (y in this.points[x]) {
+            this.points[x][y]++;
+        } else {
+            this.points[x][y] = 1;
+        }
+    } else {
+        this.points[x] = {[y]: 1};
+    }
+};
+
+/** 
+ * @param {number[]} point
+ * @return {number}
+ */
+DetectSquares.prototype.count = function(point) {
+    const [x1,y1] = point;
+    let res = 0;
+    if (!(x1 in this.points)) return 0;
+    for (let x2 in this.points) {
+        for (let y2 in this.points[x2]) {
+            const count = this.points[x2][y2];
+            if (Math.abs(x1-x2) === 0 || Math.abs(x1-x2) !== Math.abs(y1 - y2) || !this.points[x1][y2] || !this.points[x2][y1]) continue;
+            res += count * this.points[x1][y2] * this.points[x2][y1]; 
+        }
+    }
+    return res;
+};
+
+/** 
+ * Your DetectSquares object will be instantiated and called as such:
+ * var obj = new DetectSquares()
+ * obj.add(point)
+ * var param_2 = obj.count(point)
+ */
