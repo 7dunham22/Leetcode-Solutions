8d40a4a90e391473722f7807dd5f7c1a12986cbe
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-06-13 15:09:17 -0400

    Time: 2875 ms (5.84%), Space: 77.8 MB (81.02%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.js
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.js
@@ -4,10 +4,10 @@
  */
 var amountPainted = function(paint) {
     const log = [];
-    let area = new Array((5*10**4) + 1).fill(1);
-    for (let task of paint) {
+    let area = new Array(5*10**4).fill(1);
+    for (let i=0; i<paint.length; i++) {
         let day = 0;
-        const [start, end] = task;
+        const [start, end] = paint[i];
         for (let i=start; i<end; i++) {
             day += area[i];
             area[i] = 0;
