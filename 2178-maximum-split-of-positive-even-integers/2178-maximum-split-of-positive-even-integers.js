cb33d77d994f5ebc8f7dc5453070ea6f9606bf70
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-06-13 17:37:00 -0400

    Time: 343 ms (89.00%), Space: 78.2 MB (82.67%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.js
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.js
@@ -3,16 +3,23 @@
  * @return {number[]}
  */
 var maximumEvenSplit = function(finalSum) {
-    if (finalSum % 2 !== 0) return [];
+    if (finalSum % 2 !== 0 || finalSum <= 1) return [];
+    if (finalSum === 2) return [2];
+    if (finalSum === 4) return [4];
     let ans = [];
     let i = 2;
     let currSum = 0;
-    while (currSum + i <= finalSum) {
+    for (let i=2; i<finalSum; i+=2) {
         ans.push(i);
         currSum += i;
-        i += 2;
+        if (currSum >= finalSum) {
+            if (currSum > finalSum) {
+                const num = currSum - finalSum;
+                const index = (num/2)-1;
+                ans.splice(index, 1);
+            }
+            break;
+        }
     }
-    const n = ans.length;
-    ans[n-1] += finalSum - currSum;
     return ans;
 };
