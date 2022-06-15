c78052e2d5f5dba2dacff55c77e06ac570eef620
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-06-15 10:55:35 -0400

    Time: 82 ms (58.32%), Space: 44.8 MB (13.89%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.js
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.js
@@ -7,14 +7,29 @@
  */
 var findReplaceString = function(s, indices, sources, targets) {
     let res = "";
-    for (let i=0; i<s.length; i++) {
-        const j = indices.indexOf(i);
-        if (j !== -1 && sources[j] === s.slice(i, i + sources[j].length)) {
-            i += sources[j].length - 1;
-            res += targets[j];
+    const sorted = [];
+    for (let i=0; i<indices.length; i++) {
+        sorted.push([indices[i], sources[i], targets[i]]);
+    }
+    sorted.sort((a,b) => a[0] - b[0]);
+    let j=0;
+    let i=0;
+    while (i<s.length && j<sorted.length) {
+        const [index, source, target] = sorted[j];
+        const match = source === s.slice(i, i + source.length);
+        if (index === i && match) {
+            i += source.length;
+            res += target;
+            j++;
+        } else if (index === i) {
+            res += s[i];
+            i++;
+            j++;
         } else {
             res += s[i];
+            i++;
         }
     }
+    if (i < s.length) res += s.slice(i);
     return res;
 };
