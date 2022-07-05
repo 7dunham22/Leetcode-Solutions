5c739ae2740729d45537b631ed4b3d0d30ff2a10
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-07-05 14:52:46 -0400

    Time: 263 ms (30.11%), Space: 61.1 MB (44.94%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.js
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.js
@@ -3,14 +3,17 @@
  * @return {number[]}
  */
 var findBuildings = function(heights) {
-    const res = [heights.length-1];
-    let MAX = heights[heights.length-1];
-    for (let i=heights.length-2; i >= 0; i--) {
-        const height = heights[i];
-        if (height > MAX) {
-            MAX = height;
+    const n = heights.length;
+    const res = [];
+    const stack = [];
+    for (let i = n-1; i >= 0; i--) {
+        while (stack.length && heights[stack[stack.length-1]] < heights[i]) {
+            stack.pop();
+        }
+        if (stack.length === 0) {
             res.push(i);
         }
+        stack.push(i);
     }
     res.reverse();
     return res; 
