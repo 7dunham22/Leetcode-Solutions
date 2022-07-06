23ac5b7c4e0add4dd56ac8bd6219df3e93889412
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-07-06 13:26:05 -0400

    Time: 130 ms (63.64%), Space: 49.9 MB (86.26%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.js
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.js
@@ -5,14 +5,15 @@
  */
 var subarraySum = function(nums, k) {
     let count = 0;
-    const sums = {0: 1};
+    const sums = new Map();
+    sums.set(0,1);
     let sum = 0;
     for (let i=0; i<nums.length; i++) {
         sum += nums[i];
-        let prev = sums[sum - k];
+        let prev = sums.get(sum - k);
         if (prev) count += prev;
-        if (!(sum in sums)) sums[sum] = 1;
-        else sums[sum]++;
+        if (!(sums.has(sum))) sums.set(sum, 1);
+        else sums.set(sum, sums.get(sum)+1);
     }
     return count;
 };
