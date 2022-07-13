203e573142133df32355d860457261ade6ee16f0
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-07-13 17:48:20 -0400

    Time: 152 ms (12.38%), Space: 49 MB (19.05%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -3,13 +3,14 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    const chars = s.split("").map(char => char.toLowerCase());
-    const converted = chars.filter(char => {
-        const code = char.charCodeAt(0);
-        if ((code > 47 && code < 58) || (code > 96 && code < 123)) return true;
-        return false;
-    });
-    const forward = converted.join("");
-    const backward = converted.reverse().join("");
-    return forward === backward;
+    let i = 0;
+    let j = s.length-1;
+    while (i < j) {
+        while (i < j && !s[i].match(/^[A-Za-z0-9]+$/)) i += 1;
+        while (i < j && !s[j].match(/^[A-Za-z0-9]+$/)) j -= 1;
+        if (s[i].toLowerCase() !== s[j].toLowerCase()) return false;
+        i += 1;
+        j -= 1;
+    }
+    return true; 
 };
