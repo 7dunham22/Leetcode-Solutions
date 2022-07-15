d93dbfef2fdd9aabf0b1d03da919f69a8ddee39b
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-07-15 14:50:56 -0400

    Time: 189 ms (74.77%), Space: 53.3 MB (34.79%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -4,37 +4,30 @@
  */
 
 var threeSum = function(nums) {
-    const results = [];
+    const res = [];
+    let left = 0;
+    let right = nums.length - 1;
     nums.sort((a,b) => a-b);
-    const visited = new Set();
     for (let i=0; i<nums.length; i++) {
-        const num = nums[i];
-        if (num > 0) return results;
-        const target = 0 - num;
-        const rest = twoSum(nums, target, i);
-        if (rest.length > 0) {
-            for (let res of rest) {
-                const [x,y,z] = [num, res[0], res[1]];
-                const key = x+','+y+','+z;
-                if (!visited.has(key)) {
-                    results.push([x,y,z]);   
-                    visited.add(key);
-                }
-            }
-            
+        if (nums[i] > 0) return res;
+        if (nums[i] === nums[i-1]) continue;
+        left = i+1;
+        right = nums.length-1;
+        let temp = 0;
+        while (left < right) {
+            temp = nums[left] + nums[right] + nums[i];
+            if (temp === 0) {
+                res.push([nums[i], nums[left], nums[right]]);
+                left++;
+                right--;
+                while (nums[left] === nums[left-1]) left++;
+                while (nums[right] === nums[right+1]) right--;
+            } else if (temp > 0) {
+                right--;
+            } else {
+                left++;
+            } 
         }
     }
-    return results;
-};
-
-const twoSum = (nums, target, curr) => {
-    const res = [];
-    const memo = {};
-    for (let i=curr+1; i<nums.length; i++) {
-        const num = nums[i];
-        const subTarget = target - num;
-        if (subTarget in memo) res.push([subTarget, num]);
-        memo[num] = i;
-    }
     return res;
-}
+};
