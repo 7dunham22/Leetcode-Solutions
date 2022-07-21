0fb0a3ba8a72243a7d01f5ae41fa5d146b640d3e
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-07-21 14:33:09 -0400

    Time: 130 ms (20.37%), Space: 45.5 MB (23.62%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.js
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -11,14 +11,13 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    if (!head) return false;
-    let slow = head;
-    let fast = head.next;
-    while (fast && fast.next) {
-        if (slow === fast) return true;
-        slow = slow.next;
-        fast = fast.next;
-        fast = fast.next;
+    const visited = new Set();
+    while (head) {
+        if (visited.has(head)) {
+            return true;
+        }
+        visited.add(head);
+        head = head.next;
     }
     return false;
 };
