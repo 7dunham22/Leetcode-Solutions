6ad0981dea10f2766a794411a114d0e0ca8eb32e
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-07-21 15:55:29 -0400

    Time: 130 ms (42.08%), Space: 52.2 MB (51.41%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -13,30 +13,18 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function(root, p, q) {
-    if (root === p || root === q) return root;
-    const pathP = getPath(root, p);
-    const pathQ = getPath(root, q);
-    let LCA = pathP[pathP.length-1];
-    while (pathP[pathP.length-1] === pathQ[pathQ.length-1]) {
-        LCA = pathP[pathP.length-1];
-        pathP.pop();
-        pathQ.pop();
+    let pVal = p.val;
+    let qVal = q.val;
+    let node = root;
+    while (node) {
+        let parentVal = node.val;
+        if (pVal > parentVal && qVal > parentVal) {
+            node = node.right;
+        } else if (pVal < parentVal && qVal < parentVal) {
+            node = node.left;
+        } else {
+            return node;
+        }
     }
-    return LCA;
 };
 
-const getPath = (root, target) => {
-    if (!root) return null;
-    if (root === target) return [root];
-    const leftPath = getPath(root.left, target);
-    if (leftPath) {
-        leftPath.push(root);
-        return leftPath;
-    }
-    const rightPath = getPath(root.right, target);
-    if (rightPath) {
-        rightPath.push(root);
-        return rightPath;
-    }
-    return null;
-}
