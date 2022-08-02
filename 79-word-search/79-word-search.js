760401c495ce87f3fb18364d7573a00c5d6b87c5
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-08-02 11:40:30 -0400

    Time: 2586 ms (15.84%), Space: 48.3 MB (42.37%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -10,14 +10,10 @@ var exist = function(board, word) {
     const backtrack = (board, r, c, i, visiting = new Set()) => {
         const key = r + ',' + c;
         if (i === word.length) return true; 
-        if (r < 0 || r >= board.length || c < 0 || c>=board[r].length || visiting.has(key)) return false; 
-        const char = board[r][c];
-        if (char !== word[i]) return false;
+        if (r < 0 || r >= board.length || c < 0 || c>=board[r].length || visiting.has(key) || board[r][c] !== word[i]) return false; 
         visiting.add(key);
         for (let delta of deltas) {
-            let r2 = r + delta[0];
-            let c2 = c + delta[1];
-            if (backtrack(board, r2, c2, i+1, visiting)) return true;
+            if (backtrack(board, r + delta[0], c + delta[1], i+1, visiting)) return true;
         }
         visiting.delete(key);
         return false;
