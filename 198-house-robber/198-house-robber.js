61b07e17006e9d202d3aa6beeea318ef7290d917
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-08-04 16:25:48 -0400

    Time: 117 ms (8.70%), Space: 41.5 MB (96.67%) - LeetHub

--- 198-house-robber/198-house-robber.js
+++ 198-house-robber/198-house-robber.js
@@ -2,12 +2,10 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function(nums, i=0, total=0, memo={}) {
-    const key = i + ',' + total;
-    if (key in memo) return memo[key];
-    if (i >= nums.length) return total;
-    const first = nums[i];
-    const max = Math.max(rob(nums, i+2, total+first, memo), rob(nums,i+1,total,memo));
-    memo[key] = max;
-    return max;
+var rob = function(nums, i=0, memo={}) {
+    if (i >= nums.length) return 0;
+    if (i in memo) return memo[i];
+    const ans = Math.max(rob(nums,i+1,memo), rob(nums,i+2,memo) + nums[i]);
+    memo[i] = ans;
+    return ans;
 };
