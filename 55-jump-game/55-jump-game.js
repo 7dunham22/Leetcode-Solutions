c56ef9cf1f96ff6bc3a9de8916a7147e0901ed03
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-08-08 18:02:41 -0400

    Time: 1410 ms (19.84%), Space: 49.5 MB (16.04%) - LeetHub

--- 55-jump-game/55-jump-game.js
+++ 55-jump-game/55-jump-game.js
@@ -7,7 +7,7 @@ var canJump = function(nums, i=0, memo={}) {
     if (i === nums.length-1) return true;
     if (i >= nums.length) return false;
     const jumps = nums[i];
-    for (let j=1; j<=jumps; j++) {
+    for (let j=jumps; j>=1; j--) {
         if (canJump(nums, i+j, memo)) return true;
     }
     memo[i] = false;
