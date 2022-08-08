57fab8da293608daef99866290775c6dad14a3fd
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-08-08 15:44:18 -0400

    Time: 117 ms (81.67%), Space: 56.2 MB (39.67%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -4,22 +4,15 @@
  * @return {number}
  */
 var longestCommonSubsequence = function(text1, text2) {
-    const memo = {};
-    
-    const memoSolve = (p1,p2) => {
-        const key = p1 + ',' + p2;
-        if (key in memo) return memo[key];
-        if (p1 === text1.length || p2 === text2.length) return 0;
-        
-        let count = 0;
-        if (text1[p1] === text2[p2]) {
-            count = 1 + memoSolve(p1+1, p2+1);
-        } else {
-            count = Math.max(memoSolve(p1, p2+1), memoSolve(p1+1, p2));
+    const dp = new Array(text1.length+1).fill(null).map(x => new Array(text2.length+1).fill(null).map(y => 0));
+    for (let col=text2.length-1; col>=0; col--) {
+        for (let row=text1.length-1; row>=0; row--) {
+            if (text2[col] === text1[row]) {
+                dp[row][col] = 1 + dp[row+1][col+1];
+            } else {
+                dp[row][col] = Math.max(dp[row+1][col], dp[row][col+1]);
+            }
         }
-        memo[key] = count;
-        return count;
     }
-    
-    return memoSolve(0,0);
+    return dp[0][0];
 };
