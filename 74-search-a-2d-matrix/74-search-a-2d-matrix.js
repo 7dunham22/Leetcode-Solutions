787c8bccaf0685ce48c4d2c8d355584dc6f7331f
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-09-13 15:39:44 -0400

    Time: 90 ms (50.73%), Space: 41.7 MB (96.02%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    const hasValue = (nums) => {
+        if (!nums) return false;
+        let start = 0;
+        let end = nums.length-1;
+        while (start <= end) {
+            const mid = Math.floor((start + end) / 2);
+            const val = nums[mid];
+            if (val === target) {
+                return true;
+            } else if (val < target) {
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return false;
+    }
+    
+    
+    let i = 0;
+    const m = matrix[0].length;
+    const n = matrix.length;
+    while (i < n && matrix[i][m-1] < target) {
+        i++;
+    }
+    
+    const array = matrix[i];
+    return hasValue(array);
+};
