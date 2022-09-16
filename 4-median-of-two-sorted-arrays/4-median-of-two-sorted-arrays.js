ed7fc5e531a3009e17058f6c0ea1a653cd056753
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-09-16 14:48:05 -0400

    Time: 154 ms (66.88%), Space: 45.7 MB (97.12%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
@@ -4,25 +4,31 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    const shorterLength = Math.min(nums1.length, nums2.length);
-    const smaller = nums1.length === shorterLength ? nums1 : nums2; 
-    const larger = nums1.length !== shorterLength ? nums1 : nums2;
-    while (smaller.length > 0) {
-        const val = smaller.pop();
-        larger.push(val);
-        if (larger.length > 1) {
-            let i = larger.length-1;
-            while (i > 0 && larger[i-1] > larger[i]) {
-                [larger[i], larger[i-1]] = [larger[i-1], larger[i]];
-                i--;
+    let [A, B] = [nums1, nums2];
+    const total = A.length + B.length;
+    const half = Math.floor(total / 2);
+    if (B.length < A.length) {
+        [A, B] = [B, A];
+    }
+    let l = 0;
+    let r = A.length - 1;
+    while (true) {
+        const i = Math.floor((l + r) / 2);
+        const j = half - i - 2;
+        const ALeft = i >= 0 ? A[i] : Number.NEGATIVE_INFINITY;
+        const ARight = i+1 < A.length ? A[i+1] : Number.POSITIVE_INFINITY;
+        const BLeft = j >= 0 ? B[j] : Number.NEGATIVE_INFINITY;
+        const BRight = j+1 < B.length ? B[j+1] : Number.POSITIVE_INFINITY;
+        
+        if (ALeft <= BRight && BLeft <= ARight) {
+            if (total % 2) {
+                return Math.min(ARight, BRight);
             }
+            return (Math.max(ALeft, BLeft) + Math.min(ARight, BRight)) / 2;
+        } else if (ALeft > BRight) {
+            r = i - 1;
+        } else {
+            l = i + 1;
         }
     }
-    console.log(larger);
-    const mid = larger.length/2;
-    const floor = Math.floor(mid);
-    if (mid !== floor) {
-        return larger[floor];
-    }
-    return (larger[mid-1] + larger[mid]) / 2;
 };
