e896be05a72d58fa47fba53d677772e032855c33
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-09-19 11:39:49 -0400

    Time: 138 ms (25.93%), Space: 45.8 MB (23.73%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.js
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.js
@@ -11,21 +11,32 @@
  * @return {ListNode}
  */
 var reverseKGroup = function(head, k) {
-    let curr = head;
-    let i = k;
-    const nodes = [];
-    while (curr && i > 1) {
-        nodes.push(curr);
-        curr = curr.next;
-        i--;
+    const pseudo = new ListNode(null, head);
+    let groupPrev = pseudo;
+    
+    while (true) {
+        const kth = getKth(groupPrev, k);
+        if (!kth) break;
+        let groupNext = kth.next;
+        let prev = kth.next;
+        let curr = groupPrev.next;
+        while (curr !== groupNext) {
+            const temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        const temp = groupPrev.next;
+        groupPrev.next = kth;
+        groupPrev = temp;
     }
-    if (!curr) return head;
-    const next = reverseKGroup(curr.next, k);
-    head = curr;
-    while (nodes.length > 0) {
-        curr.next = nodes.pop();
-        curr = curr.next;
-    }
-    curr.next = next;
-    return head;
+    return pseudo.next;
 };
+
+const getKth = (curr, k) => {
+    while (curr && k > 0) {
+        curr = curr.next;
+        k--;
+    }
+    return curr;
+}
