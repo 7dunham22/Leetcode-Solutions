8ed0c851b4c560d2146a7d962f61411fdb0d0f6b
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-09-20 15:52:06 -0400

    Time: 213 ms (94.73%), Space: 55.6 MB (93.10%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kClosest = function(points, k) {
+    points.sort((a,b) => Math.sqrt(a[0]**2 + a[1]**2) - Math.sqrt(b[0]**2 + b[1]**2));
+    return points.slice(0, k);
+    
+};
