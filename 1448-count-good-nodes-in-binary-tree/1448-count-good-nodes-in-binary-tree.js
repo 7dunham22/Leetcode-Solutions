e728568cce6c17432c81278f26923c0ff1a41415
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-09-20 10:10:52 -0400

    Time: 259 ms (15.58%), Space: 70 MB (59.49%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
@@ -12,21 +12,13 @@
  */
 var goodNodes = function(root) {
     if (!root) return 0;
-    let count = 1;
     
     const dfs = (node, max) => {
-        if (!node) return;
-        let newMax = max;
-        if (node.val >= max) {
-            count += 1;
-            newMax = node.val;
-        }
-        dfs(node.left, newMax);
-        dfs(node.right, newMax);
+        if (!node) return 0;
+        let result = node.val >= max ? 1 : 0;
+        let newMax = Math.max(node.val, max);
+        return result + dfs(node.left, newMax) + dfs(node.right, newMax);
     }
     
-    dfs(root.left, root.val);
-    dfs(root.right, root.val);
-    return count;
-    
+    return dfs(root, root.val);
 };
