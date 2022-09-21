024ed0172ff53c4653c3ce41fc8fa58f11921cae
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-09-21 12:38:35 -0400

    Time: 76 ms (91.55%), Space: 44.7 MB (32.05%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.js
+++ 40-combination-sum-ii/40-combination-sum-ii.js
@@ -5,25 +5,30 @@
  */
 var combinationSum2 = function(candidates, target) {
     candidates.sort((a,b) => a-b);
-    const res = [];
     
-    const backtrack = (curr, pos, target) => {
-        if (target === 0) {
-            res.push([...curr]);
-            return;
-        }
-        if (target < 0) return;
+    const backtrack = (target, i) => {
+        // If target matched, return array with one empty array element. The previously chosen value will be added to complete the choice.
+        if (target === 0) return [[]];
         
-        let prev = -1;
-        for (let i=pos; i<candidates.length; i++) {
-            if (candidates[i] === prev) continue;
-            curr.push(candidates[i]);
-            backtrack(curr, i + 1, target - candidates[i]);
-            curr.pop();
-            prev = candidates[i];
+        // If no target is ever met, return an empty array. No previous choices will be included. 
+        if (target < 0 || i === candidates.length) return [];
+        
+        // If we skip one value, skip each following proceeding value. This step avoids duplication. 
+        let nextIndex = i+1;
+        while (nextIndex<candidates.length && candidates[i] === candidates[nextIndex]) {
+            nextIndex += 1;
         }
+        
+        // The result array is initialized with the choices of skipping the current value. 
+        const res = backtrack(target, nextIndex);
+        
+        // Include the results of choosing the current value. 
+        const takeOne = backtrack(target - candidates[i], i+1);
+        for (const combo of takeOne) {
+            res.push([candidates[i], ...combo]);
+        }
+        return res;
     }
     
-    backtrack([], 0, target);
-    return res;
+    return backtrack(target, 0);
 };
