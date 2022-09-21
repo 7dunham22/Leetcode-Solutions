d49d13d0ee662d3c970568bf4346223248d2eac9
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-09-21 18:45:12 -0400

    Time: 91 ms (92.62%), Space: 47.9 MB (42.56%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.js
+++ 130-surrounded-regions/130-surrounded-regions.js
@@ -3,31 +3,41 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 var solve = function(board) {
+    const n = board.length;
+    const m = board[0].length;
     
-    const canEscape = (r,c, visited) => {
-        const key = r+','+c;
-        if (board[r][c] === 'X' || visited.has(key)) return false;
-        if (r===0 || r===board.length-1 || c===0 || c===board[0].length-1) return true;
-        visited.add(key);
-        const up = canEscape(r-1, c, visited);
-        const left = canEscape(r, c-1, visited);
-        const down = canEscape(r+1, c, visited);
-        const right = canEscape(r, c+1, visited);
-        return (up || left || down || right);
+    const save = (r, c) => {
+        if (r<0 || r===board.length || c<0 || c===board[0].length || board[r][c] !== 'O') return;
+        board[r][c] = 'T';
+        save(r-1,c);
+        save(r+1,c);
+        save(r,c-1);
+        save(r,c+1);
     }
     
-    const fill = (visited) => {
-        for (const key of [...visited]) {
-            const [r, c] = key.split(',');
-            board[r][c] = 'X';
+    //save
+    for (let r=0; r<n; r++) {
+        for (let c=0; c<m; c++) {
+            if (board[r][c] === 'O' && ([0,n-1].includes(r) || [0,m-1].includes(c))) {
+                save(r,c);   
+            }
         }
     }
     
-    for (let r=0; r<board.length; r++) {
-        for (let c=0; c<board[0].length; c++) {
-            const visited = new Set();
-            if (!canEscape(r,c, visited)) {
-                fill(visited);
+    //capture
+    for (let r=0; r<n; r++) {
+        for (let c=0; c<m; c++) {
+            if (board[r][c] === 'O') {
+                board[r][c] = 'X';
+            }
+        }
+    }
+    
+    //Return saved to O
+    for (let r=0; r<n; r++) {
+        for (let c=0; c<m; c++) {
+            if (board[r][c] === 'T') {
+                board[r][c] = 'O';
             }
         }
     }
