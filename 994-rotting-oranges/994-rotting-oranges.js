5219a13f78a30a4881a52f75afd924012fe07dbc
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-09-22 10:10:32 -0400

    Time: 162 ms (14.08%), Space: 46.5 MB (18.94%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.js
+++ 994-rotting-oranges/994-rotting-oranges.js
@@ -6,18 +6,16 @@ var orangesRotting = function(grid) {
     const n = grid.length;
     const m = grid[0].length;
     let rotten = []; // Queue of the oranges already rotted. [r, c]
-    const visiting = new Set();
     let mins = 0;
     
     const makeRot = () => {
         while (rotten.length > 0) {
             const [r,c,min] = rotten.shift();
             if (min > mins) mins = min;
-            grid[r][c] = 2;
             const neighbors = [[r-1,c], [r+1,c], [r,c-1], [r,c+1]];
             for (const [r1,c1] of neighbors) {
-                if (r1<0 || r1===n || c1<0 || c1===m || grid[r1][c1] !== 1 || visiting.has(r1+','+c1)) continue;
-                visiting.add(r1+','+c1);
+                if (r1<0 || r1===n || c1<0 || c1===m || grid[r1][c1] !== 1) continue;
+                grid[r1][c1] = 2;
                 rotten.push([r1,c1,min+1]);
             }
         }
