e03d95636a6d724c1e5d57f6ab1b4654f8d01ca8
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-09-26 13:42:52 -0400

    Time: 109 ms (38.99%), Space: 42.2 MB (91.38%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -3,14 +3,8 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-    const memo = {};
-    
-    const getStep = (i) => {
-        if (i >= cost.length) return 0;
-        if (i in memo) return memo[i];
-        memo[i] = cost[i] + Math.min(getStep(i+1), getStep(i+2));
-        return memo[i];
+    for (let i=cost.length-3; i>=0; i--) {
+        cost[i] += Math.min(cost[i+1], cost[i+2]);
     }
-    
-    return Math.min(getStep(0), getStep(1));
+    return Math.min(cost[0], cost[1]);
 };
