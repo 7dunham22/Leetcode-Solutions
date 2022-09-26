56bc6b827d9914177b8debe9bccad948cf4a8a21
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-09-26 14:30:26 -0400

    Time: 748 ms (27.59%), Space: 108.3 MB (14.28%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.js
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.js
@@ -7,18 +7,19 @@ var canPartition = function(nums) {
     if (sum%2) return false;
     
     const target = Math.floor(sum / 2);
+    const n = nums.length;
     
     const memo = {};
     
-    const dfs = (i=0, prev=0) => {
-        if (prev === target) return true;
-        if (i===nums.length || prev > target) return false;
-        const key = i + ',' + prev;
+    const dfs = (n, target) => {
+        const key = n + ',' + target;
         if (key in memo) return memo[key];
-        const result = dfs(i+1, prev) || dfs(i+1, prev + nums[i]);
+        if (target === 0) return true;
+        if (n===0 || target < 0) return false;
+        const result = dfs(n-1, target - nums[n-1]) || dfs(n-1, target);
         memo[key] = result;
         return result;
     }
     
-    return dfs();
+    return dfs(n-1, target);
 };
