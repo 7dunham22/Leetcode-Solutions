c59ee688136642989d123ab072a667fbb03eb9ac
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-09-27 09:37:53 -0400

    Time: 117 ms (29.90%), Space: 44.2 MB (60.29%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.js
+++ 252-meeting-rooms/252-meeting-rooms.js
@@ -3,16 +3,13 @@
  * @return {boolean}
  */
 var canAttendMeetings = function(intervals) {
-    if (intervals.length === 0) return true;
+    if (intervals.length < 2) return true;
     intervals.sort((a,b) => a[0] - b[0]);
-    let prevEnd = intervals[0][1];
+    let prev = intervals[0][1];
     for (let i=1; i<intervals.length; i++) {
-        const [start, end] = intervals[i];
-        if (start >= prevEnd) {
-            prevEnd = end;
-        } else {
-            return false;
-        }
+        const start = intervals[i][0];
+        if (start < prev) return false;
+        prev = intervals[i][1];
     }
     return true;
 };
