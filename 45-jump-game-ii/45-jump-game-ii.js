bac6ec8289d0bf124ca7468238bee4297769ddee
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-09-28 12:03:32 -0400

    Time: 117 ms (48.66%), Space: 43.6 MB (90.99%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.js
+++ 45-jump-game-ii/45-jump-game-ii.js
@@ -8,7 +8,7 @@ var jump = function(nums) {
     let res = 0;
     while (r < nums.length-1) {
         let maxJump = 0;
-        for (let i=0; i<=r; i++) {
+        for (let i=l; i<=r; i++) {
             maxJump = Math.max(maxJump, i + nums[i]);
         }
         l = r+1;
