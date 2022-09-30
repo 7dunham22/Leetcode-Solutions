727d1c3d476b5a834269524a6bef55abf68477e0
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-09-30 14:00:11 -0400

    Time: 129 ms (41.47%), Space: 44.2 MB (86.84%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.js
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.js
@@ -5,17 +5,19 @@
 var minMeetingRooms = function(intervals) {
     const starts = intervals.map(x => x[0]).sort((a,b) => a-b);
     const ends = intervals.map(x => x[1]).sort((a,b) => a-b);
-    
-    let minRooms = 0;
+    let i = 0;
     let j = 0;
-    
-    for (let i=0; i<starts.length; i++) {
+    let count = 0;
+    let res = 0;
+    while (i < starts.length) {
         if (starts[i] < ends[j]) {
-            minRooms += 1;
+            i++;
+            count++;
         } else {
-            j += 1;
+            j++;
+            count--;
         }
+        res = Math.max(res,count);
     }
-    
-    return minRooms;
+    return res;
 };
