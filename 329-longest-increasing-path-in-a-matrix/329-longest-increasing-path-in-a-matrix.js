30b46ec332a5f1d3e98e5e4e3288064ee3dab33b
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-09-30 13:16:50 -0400

    Time: 439 ms (9.23%), Space: 53.5 MB (12.24%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.js
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.js
@@ -6,19 +6,17 @@ var longestIncreasingPath = function(matrix) {
     const memo = {};
     let MAX = 0;
     
-    const dfs = (r,c, prev = null, visiting = new Set()) => {
+    const dfs = (r,c, prev = null) => {
         const pos = r+','+c;
-        if (r<0 || r===matrix.length || c<0 || c===matrix[0].length || visiting.has(pos) || (prev !== null && matrix[r][c] <= prev)) {
+        if (r<0 || r===matrix.length || c<0 || c===matrix[0].length || (prev !== null && matrix[r][c] <= prev)) {
             return 0;
         }
         if (pos in memo) return memo[pos];
         let len = 1;
-        visiting.add(pos);
         const neighbors = [[r+1,c], [r-1,c], [r,c+1], [r,c-1]];
         for (const [r2,c2] of neighbors) {
-            len = Math.max(len, 1 + dfs(r2,c2,matrix[r][c],visiting));
+            len = Math.max(len, 1 + dfs(r2,c2,matrix[r][c]));
         }
-        visiting.delete(pos);
         memo[pos] = len;
         return len;
     }
