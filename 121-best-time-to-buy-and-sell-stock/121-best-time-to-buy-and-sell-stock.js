f41b56c4f4e06b37237e53bd772024c1dc45ebc9
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-09-30 14:34:18 -0400

    Time: 136 ms (49.92%), Space: 51.9 MB (36.93%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -4,11 +4,14 @@
  */
 var maxProfit = function(prices) {
     let maxProfit = 0;
+    let minPrice = prices[0];
     let j = 0;
     for (let i=1; i<prices.length; i++) {
-        maxProfit = Math.max(maxProfit, prices[i] - prices[j]);
-        if (prices[i] - prices[j] <= 0) {
-            j = i;
+        const price = prices[i];
+        const profit = price - minPrice;
+        maxProfit = Math.max(maxProfit, profit);
+        if (price < minPrice) {
+            minPrice = price;
         }
     }
     return maxProfit;
