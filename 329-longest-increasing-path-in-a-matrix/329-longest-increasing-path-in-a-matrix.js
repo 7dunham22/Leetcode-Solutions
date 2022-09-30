06631c5dd722e319c6f11b29d6028b4dc39f45ee
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-09-30 13:15:51 -0400

    Time: 443 ms (9.04%), Space: 56.5 MB (8.29%) - LeetHub

---
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var longestIncreasingPath = function(matrix) {
+    const memo = {};
+    let MAX = 0;
+    
+    const dfs = (r,c, prev = null, visiting = new Set()) => {
+        const pos = r+','+c;
+        if (r<0 || r===matrix.length || c<0 || c===matrix[0].length || visiting.has(pos) || (prev !== null && matrix[r][c] <= prev)) {
+            return 0;
+        }
+        if (pos in memo) return memo[pos];
+        let len = 1;
+        visiting.add(pos);
+        const neighbors = [[r+1,c], [r-1,c], [r,c+1], [r,c-1]];
+        for (const [r2,c2] of neighbors) {
+            len = Math.max(len, 1 + dfs(r2,c2,matrix[r][c],visiting));
+        }
+        visiting.delete(pos);
+        memo[pos] = len;
+        return len;
+    }
+    
+    for (let r=0; r<matrix.length; r++) {
+        for (let c=0; c<matrix[0].length; c++) {
+            const path = dfs(r,c);
+            MAX = Math.max(MAX, path);
+        }
+    }
+    
+    return MAX;
+};
