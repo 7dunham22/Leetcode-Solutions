75bb023287290bf83cae13cdd4226b7dc38b46e1
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-10-01 15:41:32 -0400

    Time: 322 ms (35.35%), Space: 56 MB (19.03%) - LeetHub

--- 705-design-hashset/705-design-hashset.js
+++ 705-design-hashset/705-design-hashset.js
@@ -1,6 +1,68 @@
+function Node(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+}
+
+function insert(root, val) {
+    if (!root) return new Node(val);
+    if (val === root.val) {
+        return root;
+    }
+    if (val < root.val) {
+        if (!root.left) {
+            root.left = new Node(val);
+        } else {
+            root.left = insert(root.left, val);
+        }
+    } else {
+        if (!root.right) {
+            root.right = new Node(val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+    }
+    return root;
+}
+
+function remove(root, val) {
+    if (!root) return root;
+    if (val < root.val) {
+        root.left = remove(root.left, val);
+    } else if (val > root.val) {
+        root.right = remove(root.right, val);
+    } else {
+        if (!root.left) {
+            return root.right;
+        }
+        if (!root.right) {
+            return root.left;
+        } else {
+            root.val = minValue(root.right);
+            root.right = remove(root.right, root.val);
+        }
+    }
+    return root;
+}
+
+function minValue(root) {
+    if (!root) return root;
+    while (root.left) {
+        root = root.left;
+    }
+    return root.val;
+}
+
+function contains(root, val) {
+    if (!root) return false;
+    if (val < root.val) return contains(root.left, val);
+    if (val > root.val) return contains(root.right, val);
+    return true;
+}
+
 
 var MyHashSet = function() {
-    this.hashSet = new Set();
+    this.pseudo = new Node(-1);
 };
 
 /** 
@@ -8,7 +70,7 @@ var MyHashSet = function() {
  * @return {void}
  */
 MyHashSet.prototype.add = function(key) {
-    this.hashSet.add(key);
+    this.pseudo.right = insert(this.pseudo.right, key);
 };
 
 /** 
@@ -16,7 +78,7 @@ MyHashSet.prototype.add = function(key) {
  * @return {void}
  */
 MyHashSet.prototype.remove = function(key) {
-    this.hashSet.delete(key);
+    this.pseudo.right = remove(this.pseudo.right, key);
 };
 
 /** 
@@ -24,7 +86,7 @@ MyHashSet.prototype.remove = function(key) {
  * @return {boolean}
  */
 MyHashSet.prototype.contains = function(key) {
-    return this.hashSet.has(key);
+    return contains(this.pseudo.right, key);
 };
 
 /** 
