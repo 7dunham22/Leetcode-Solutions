041dcaa5e19414e9925edffce82211d159533751
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-10-04 08:54:39 -0400

    Time: 130 ms (89.67%), Space: 56.3 MB (58.28%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.js
+++ 575-distribute-candies/575-distribute-candies.js
@@ -6,7 +6,7 @@ var distributeCandies = function(candyType) {
     const maxCandies = candyType.length/2;
     const types = new Set();
     for (const candy of candyType) {
-        if (!types.has(candy)) types.add(candy);
+        types.add(candy);
     }
     
     return Math.min(maxCandies, types.size);
