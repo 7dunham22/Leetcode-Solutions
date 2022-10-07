91d9bcdddebc5152de8af29dcbfeb8a648a576e9
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-10-07 12:55:48 -0400

    Time: 554 ms (14.03%), Space: 57.9 MB (13.38%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var removeDuplicates = function(s, k) {
+    const stack = [];
+    for (let i=0; i<s.length; i++) {
+        const char = s[i];
+        if (stack.length && char === stack[stack.length-1][0]) {
+            stack[stack.length-1] += char;
+        } else {
+            stack.push(char);
+        }
+        if (stack[stack.length-1].length >= k) {
+            stack.pop();
+        }
+    }
+    return stack.join("");
+};
