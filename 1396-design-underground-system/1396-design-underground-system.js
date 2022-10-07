dd72b9820d28853538d38c114201070d8e2774f9
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-10-07 11:14:28 -0400

    Time: 477 ms (8.36%), Space: 59.9 MB (44.48%) - LeetHub

---
+++ 1396-design-underground-system/1396-design-underground-system.js
@@ -0,0 +1,47 @@
+
+var UndergroundSystem = function() {
+    this.customers = {}; // {userId: {entry: stationName, time: t}}
+    this.travelTimes = {}; // {startStation: {endStation: [...travel times]}}
+};
+
+/** 
+ * @param {number} id 
+ * @param {string} stationName 
+ * @param {number} t
+ * @return {void}
+ */
+UndergroundSystem.prototype.checkIn = function(id, stationName, t) {
+    if (!(id in this.customers)) this.customers[id] = {};
+    this.customers[id] = {'entryStation': stationName, 'entryTime': t};
+};
+
+/** 
+ * @param {number} id 
+ * @param {string} stationName 
+ * @param {number} t
+ * @return {void}
+ */
+UndergroundSystem.prototype.checkOut = function(id, exitStation, t) {
+    const {entryStation, entryTime} = this.customers[id];
+    if (!(entryStation in this.travelTimes)) this.travelTimes[entryStation] = {};
+    if (!(exitStation in this.travelTimes[entryStation])) this.travelTimes[entryStation][exitStation] = [];
+    this.travelTimes[entryStation][exitStation].push(t - entryTime);
+};
+
+/** 
+ * @param {string} startStation 
+ * @param {string} endStation
+ * @return {number}
+ */
+UndergroundSystem.prototype.getAverageTime = function(startStation, endStation) {
+    const travelTimes = this.travelTimes[startStation][endStation];
+    return travelTimes.reduce((prev,curr) => prev+curr) / travelTimes.length;
+};
+
+/** 
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * var obj = new UndergroundSystem()
+ * obj.checkIn(id,stationName,t)
+ * obj.checkOut(id,stationName,t)
+ * var param_3 = obj.getAverageTime(startStation,endStation)
+ */
