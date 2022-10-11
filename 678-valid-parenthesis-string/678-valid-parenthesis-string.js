8ba2885ab98ddfacaea961fed9163300637e750c
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-10-11 12:35:36 -0400

    Time: 89 ms (57.73%), Space: 42.1 MB (42.73%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.js
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.js
@@ -3,34 +3,14 @@
  * @return {boolean}
  */
 var checkValidString = function(s) {
-    const memo = {};
-    
-    const dfs = (i=0, balance=0) => {
-        if (i === s.length) return balance === 0;
-        const key = i + ',' + balance;
-        if (key in memo) return memo[key];
+    let lo = 0;
+    let hi = 0;
+    for (let i=0; i<s.length; i++) {
         const char = s[i];
-        if (char === '(') {
-            memo[key] = dfs(i+1, balance+1);
-        } else if (char === ')') {
-            if (balance === 0) {
-                memo[key] = false;
-            } else {
-                memo[key] = dfs(i+1, balance-1);
-            }
-        } else {
-            if (dfs(i+1, balance)) {
-                memo[key] = true;
-            } else if (dfs(i+1, balance+1)) {
-                memo[key] = true;
-            } else if (balance > 0 && dfs(i+1, balance-1)) {
-                memo[key] = true;
-            } else {
-                memo[key] = false;   
-            }
-        }
-        return memo[key];
+        lo += char === '(' ? 1 : -1;
+        hi += char !== ')' ? 1 : -1;
+        if (hi < 0) break;
+        lo = Math.max(lo, 0);
     }
-    
-    return dfs();
+    return lo === 0;
 };
