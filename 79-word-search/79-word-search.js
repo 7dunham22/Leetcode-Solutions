c85786731b391156e4ca5f7134eff8ea392235a5
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-10-12 14:26:15 -0400

    Time: 4159 ms (7.33%), Space: 48.4 MB (43.77%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -4,26 +4,24 @@
  * @return {boolean}
  */
 var exist = function(board, word) {
+    const visited = new Set();
     
-    const deltas = [[1,0], [0,1], [-1,0], [0,-1]];
-    
-    const backtrack = (board, r, c, i) => {
-        const key = r + ',' + c;
-        if (i === word.length) return true; 
-        if (r < 0 || r >= board.length || c < 0 || c>=board[r].length || board[r][c] !== word[i]) return false; 
-        const char = board[r][c];
-        board[r][c] = "#"
-        for (let delta of deltas) {
-            if (backtrack(board, r + delta[0], c + delta[1], i+1)) return true;
+    const dfs = (r, c, i = 0) => {
+        if (i === word.length) return true;
+        if (r<0 || r===board.length || c<0 || c===board[0].length || visited.has(r+','+c)) return false;
+        if (board[r][c] === word[i]) {
+            visited.add(r+','+c);
+            if (dfs(r+1,c,i+1) || dfs(r-1,c,i+1) || dfs(r,c+1,i+1) || dfs(r,c-1,i+1)) return true;
         }
-        board[r][c] = char;
+        visited.delete(r+','+c);
         return false;
     }
     
     for (let r=0; r<board.length; r++) {
-        for (let c=0; c<board[r].length; c++) {
-            if (backtrack(board, r, c, 0)) return true;
+        for (let c=0; c<board[0].length; c++) {
+            if (dfs(r,c)) return true;
         }
     }
+    
     return false;
 };
