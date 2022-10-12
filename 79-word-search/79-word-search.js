4f975fd3339d7af338e87d0ff1971f94d31f13f9
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-10-12 14:35:10 -0400

    Time: 3656 ms (9.84%), Space: 48.4 MB (39.41%) - LeetHub

--- 79-word-search/79-word-search.js
+++ 79-word-search/79-word-search.js
@@ -8,11 +8,9 @@ var exist = function(board, word) {
     
     const dfs = (r, c, i = 0) => {
         if (i === word.length) return true;
-        if (r<0 || r===board.length || c<0 || c===board[0].length || visited.has(r+','+c)) return false;
-        if (board[r][c] === word[i]) {
-            visited.add(r+','+c);
-            if (dfs(r+1,c,i+1) || dfs(r-1,c,i+1) || dfs(r,c+1,i+1) || dfs(r,c-1,i+1)) return true;
-        }
+        if (r<0 || r===board.length || c<0 || c===board[0].length || visited.has(r+','+c) || board[r][c] !== word[i]) return false;
+        visited.add(r+','+c);
+        if (dfs(r+1,c,i+1) || dfs(r-1,c,i+1) || dfs(r,c+1,i+1) || dfs(r,c-1,i+1)) return true;
         visited.delete(r+','+c);
         return false;
     }
