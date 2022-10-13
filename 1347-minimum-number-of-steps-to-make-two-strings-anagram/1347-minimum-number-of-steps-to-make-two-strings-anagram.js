f1b1e6fc6a72881ce318e0e6a1942cf4bbec5342
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-10-13 11:34:12 -0400

    Time: 356 ms (10.00%), Space: 47.5 MB (73.67%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var minSteps = function(s, t) {
+    const sCounts = {};
+    const tCounts = {};
+    for (let i=0; i<s.length; i++) {
+        const s1 = s[i];
+        const t1 = t[i];
+        if (!(s1 in sCounts)) sCounts[s1] = 0;
+        if (!(s1 in tCounts)) tCounts[s1] = 0;
+        if (!(t1 in tCounts)) tCounts[t1] = 0;
+        if (!(t1 in sCounts)) sCounts[t1] = 0;
+        sCounts[s1] += 1;
+        tCounts[t1] += 1;
+    }
+    
+    let diff = 0;
+    for (const char in sCounts) {
+        diff += Math.abs(sCounts[char] - tCounts[char]);
+    }
+    
+    return diff / 2;
+    
+};
