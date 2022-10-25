1fcc7b3e90e211cb786f0733162d445a1f055455
David Dunham <39914937+7dunham22@users.noreply.github.com>
2022-10-25 13:33:15 -0400

    Time: 120 ms (49.90%), Space: 44.8 MB (43.48%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.js
+++ 867-transpose-matrix/867-transpose-matrix.js
@@ -5,7 +5,7 @@
 var transpose = function(matrix) {
     const n = matrix.length;
     const m = matrix[0].length;
-    const ans = new Array(m).fill(null).map(x => new Array(n).fill(null));
+    const ans = new Array(m).fill(null).map(x => new Array(n));
     for (let r=0; r<n; r++) {
         for (let c=0; c<m; c++) {
             ans[c][r] = matrix[r][c];
